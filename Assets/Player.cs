394c485560a2ce1b9b9bfd30afbe8599cfca7027
hktrollgames <hktrollgames@gmail.com>
2021-07-30 20:46:03 +0900

    스테이지 1추가

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) move.z = 1;
+        if (Input.GetKey(KeyCode.S)) move.z = -1;
+        if (Input.GetKey(KeyCode.A)) move.x = -1;
+        if (Input.GetKey(KeyCode.D)) move.x = 1;
+        if (move != Vector3.zero)
+        {
+            move.Normalize();
+            transform.Translate(move * speed * Time.deltaTime, Space.World);
+            transform.forward = move;
+        }
     }
+    public float speed = 5;
 }
--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -1,18 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    NavMeshAgent agent;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = target.position;
     }
 }
