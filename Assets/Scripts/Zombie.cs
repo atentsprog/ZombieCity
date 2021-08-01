3999df3f07682f9c460908151d9d15e579667cb8
hktrollgames <hktrollgames@gmail.com>
2021-08-02 08:16:43 +0900

    좀비 총에 맞으면 피격 이펙트 생성

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -15,28 +16,50 @@ public class Zombie : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         target = FindObjectOfType<Player>().transform;  // 
 
-        while(hp > 0)
+        originalMoveSpeed = agent.speed;
+        while (hp > 0)
         {
             if (target)
                 agent.destination = target.position;
             yield return new WaitForSeconds(Random.Range(0.5f, 2f));
         }
     }
-
     internal void TakeHit(int damage)
     {
         hp -= damage;
-        animator.Play("TakeHit");
+        animator.Play("TakeHit", 0, 0);
+        agent.speed = 0;
+        Invoke(nameof(RestoreSpeed), 0.2f);
+
         // 피격 이펙트 생성(피,..)
+        CreateTakeHitEffect();
+
         if (hp <= 0)
         {
+            CancelInvoke(nameof(RestoreSpeed));
             GetComponent<Collider>().enabled = false;
-            Invoke(nameof(Die), 1);
+            Invoke(nameof(Die), dieAnimationPlayTime);
         }
     }
+    public float dieAnimationPlayTime = 0.6f;
+
+    float originalMoveSpeed;
+    private void RestoreSpeed()
+    {
+        agent.speed = originalMoveSpeed;
+    }
+     
+    public GameObject takeHitEffects;
+    public float takeHitEffectsYOffset = 1f;
+    private void CreateTakeHitEffect()
+    {
+        Instantiate(takeHitEffects, transform.position + new Vector3(0, takeHitEffectsYOffset, 0), Quaternion.identity, transform);
+    }
+
+    public float dieDestroyTime = 3;
     void Die()
     {
         animator.Play("Die");
-        Destroy(gameObject, 1);
+        Destroy(gameObject, dieDestroyTime);
     }
 }
