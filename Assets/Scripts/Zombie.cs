9a189c154fff4e9567b4dcd81f932c3ec3073cc8
hktrollgames <hktrollgames@gmail.com>
2021-08-02 18:20:49 +0900

    뒤로 밀려남

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -9,13 +9,15 @@ public class Zombie : MonoBehaviour
     NavMeshAgent agent;
     Animator animator;
     public int hp = 100;
+    float originalSpeed;
     IEnumerator Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponentInChildren<Animator>();
         target = FindObjectOfType<Player>().transform;  // 
+        originalSpeed = agent.speed;
 
-        while(hp > 0)
+        while (hp > 0)
         {
             if (target)
                 agent.destination = target.position;
@@ -23,18 +25,45 @@ public class Zombie : MonoBehaviour
         }
     }
 
-    internal void TakeHit(int damage)
+    internal void TakeHit(int damage, Vector3 toMoveDirection)
     {
         hp -= damage;
         //animator.Play("TakeHit");
         animator.Play(Random.Range(0, 2) == 0 ? "TakeHit1" : "TakeHit2", 0, 0);
         // 피격 이펙트 생성(피,..)
+
+        // 뒤로 밀려나게하자.
+        PushBackMove(toMoveDirection);
+
+        // 이동 스피드를 잠시 0으로 만들자.
+        agent.speed = 0;
+        CancelInvoke(nameof(SetTakeHitSpeed));
+        Invoke(nameof(SetTakeHitSpeed), TakeHitStopSpeedTime);
+
         if (hp <= 0)
         {
             GetComponent<Collider>().enabled = false;
             Invoke(nameof(Die), 1);
         }
     }
+
+    public float moveBackDistance = 0.1f;
+    public float moveBackNoise = 0.1f;
+    private void PushBackMove(Vector3 toMoveDirection)
+    {
+        toMoveDirection.x += Random.Range(-moveBackNoise, moveBackNoise);
+        toMoveDirection.z += Random.Range(-moveBackNoise, moveBackNoise);
+        toMoveDirection.y = 0;
+        toMoveDirection.Normalize();
+        transform.Translate(toMoveDirection * moveBackDistance, Space.World);
+    }
+
+    public float TakeHitStopSpeedTime = 0.1f;
+    private void SetTakeHitSpeed()
+    {
+        agent.speed = originalSpeed;
+    }
+
     void Die()
     {
         animator.Play("Die");
