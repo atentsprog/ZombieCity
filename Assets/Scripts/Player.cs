bf7eaf91719d540e53d9d7636e34f7125d216310
hktrollgames <hktrollgames@gmail.com>
2021-08-02 17:01:13 +0900

    마우스 클릭으로 총알 발사하게함, 상하체 다른 애니메이션 재생

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,42 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
+        LookAtMouse();
+        Move();
+        Fire();
+    }
+    Plane plane = new Plane(new Vector3(0, 1, 0), 0);
+
+    private void LookAtMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (plane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            Vector3 dir = hitPoint - transform.position;
+            dir.y = transform.position.y;
+            dir.Normalize();
+            transform.forward = dir;
+        }
+    }
+
+    private void Fire()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            //animator.Play("Shoot");
+            animator.SetBool("Fire", true);
+            Instantiate(bullet, bulletPosition.position, transform.rotation);
+        }
+        else
+        {
+            animator.SetBool("Fire", false);
+        }
+    }
+
+    private void Move()
+    {
         Vector3 move = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) move.z = 1;
         if (Input.GetKey(KeyCode.S)) move.z = -1;
@@ -20,19 +57,15 @@ public class Player : MonoBehaviour
         {
             move.Normalize();
             transform.Translate(move * speed * Time.deltaTime, Space.World);
-            transform.forward = move;
+            //transform.forward = move;
         }
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            animator.Play("Shoot");
-            Instantiate(bullet, bulletPosition.position, transform.rotation);
-        }
 
         animator.SetFloat("DirX", move.x);
         animator.SetFloat("DirY", move.z);
         animator.SetFloat("Speed", move.sqrMagnitude);
     }
+
     public float speed = 5;
     public GameObject bullet;
     public Transform bulletPosition;
