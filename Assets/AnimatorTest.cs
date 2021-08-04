11036ed389d2141a65e014432af14d3ab76e1cf4
hktrollgames <hktrollgames@gmail.com>
2021-08-04 13:40:53 +0900

    샷건 적용

---
+++ Assets/AnimatorTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorTest : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            animator.Play("ShotGunFire", 0, 0);
+    }
+}
