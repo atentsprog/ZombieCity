24c175bf88f5c6fb95cf5f72a95fe539a34cff8f
hktrollgames <hktrollgames@gmail.com>
2021-08-04 20:56:11 +0900

    UI 추가중

---
+++ Assets/StageManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : SingletonMonoBehavior<StageManager>
+{
+    public SaveInt highScore;
+    public int score;
+
+    new private void Awake()
+    {
+        base.Awake();
+        highScore = new SaveInt("highScore");
+    }
+    public void AddScore(int addScore)
+    {
+        score += addScore;
+
+        if (highScore.Value < score)
+            highScore.Value = score;
+
+        UIRefresh();
+    }
+    private void UIRefresh()
+    {
+
+    }
+}
