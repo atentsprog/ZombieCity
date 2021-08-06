f2b60c44d689b5794a1f4d2f5e312ba3bec0b1ad
hktrollgames <hktrollgames@gmail.com>
2021-08-06 16:35:30 +0900

    솔져 움직이는 방향 버그 수정

---
+++ Assets/LightManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    // 환경관
+    public Color dayColor;
+    public Color nightColor;
+}
