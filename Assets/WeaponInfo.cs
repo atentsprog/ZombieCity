60675741b920e76702ea48197237b3c86bc0c327
hktrollgames <hktrollgames@gmail.com>
2021-08-06 11:22:14 +0900

    웨폰 정보 추가중

--- Assets/WeaponInfo.cs
+++ Assets/WeaponInfo.cs
@@ -10,4 +10,12 @@ public class WeaponInfo : ScriptableObject
     public int damage = 20;
     public AnimatorOverrideController overrideAnimator;
     public GameObject weaponGo;
+
+
+    public GameObject bullet;
+    public Transform bulletPosition;
+    public GameObject bulletLight;
+
+    public float delay = 0.2f;
+    public int maxBulletCount = 6;
 }
