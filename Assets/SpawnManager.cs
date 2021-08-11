1c4a755e8bdfc3cef695b00f286c831ebb347faa
hktrollgames <hktrollgames@gmail.com>
2021-08-11 13:42:12 +0900

    웨이브 적용

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -7,8 +7,16 @@ public class SpawnManager : MonoBehaviour
 {
     /// 특정시간마다 웨이브 진행
     /// 몬스터가 다 죽어도 다음 웨이브 진행
-    //public float waveInterval = 60f;
-    //public float waveWaitTime = 3f;
+    public float nextWaveWaitTime = 3f;
+
+    [System.Serializable]
+    public class WaveInfo
+    {
+        public int spawnCount = 10;
+        public float waveInterval = 60f;
+        public List<SpawnInfo> spawnInfos;
+    }
+    public List<WaveInfo> waveInfo;
 
     [System.Serializable]
     public class SpawnInfo
@@ -17,21 +25,42 @@ public class SpawnManager : MonoBehaviour
         public float ratio;
     }
 
-    public List<SpawnInfo> spawnInfos;
-    public int spawnCount = 10;
 
     public SpawnPoint[] spawnPoints;
 
+    public float waveEndTime;
     private IEnumerator Start()
     {
         spawnPoints = GetComponentsInChildren<SpawnPoint>(true);
+
+        foreach(var item in waveInfo)
+        {
+            yield return SpawnWaveMonsterCo(item);
+            waveEndTime = item.waveInterval + Time.time;
+            while (waveEndTime > Time.time)
+                yield return null;
+
+            // 다음 웨이브 시작했습니다. <- 알림 UI 추가하자.
+            Debug.Log("다음 웨이브 시작했습니다.");
+            yield return new WaitForSeconds(nextWaveWaitTime);
+        }
+
+        Debug.Log("모든 웨이브가 진행되었습니다.");
+    }
+
+    public float randomSpawnDelay = 1f;
+    private IEnumerator SpawnWaveMonsterCo(WaveInfo waveInfo)
+    {
+        int spawnCount = waveInfo.spawnCount;
+        List<SpawnInfo> spawnInfos = waveInfo.spawnInfos;
         for (int i = 0; i < spawnCount; i++)
         {
             int randomIndex = Random.Range(0, spawnPoints.Length);
             var point = spawnPoints[randomIndex].transform.position;
             Zombie regenMonster = spawnInfos.OrderBy(x => Random.Range(0, x.ratio)).Last().go;
             Instantiate(regenMonster, point, Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(0, randomSpawnDelay));
         }
-        yield return null;
     }
 }
