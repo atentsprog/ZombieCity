b109c410410510ae01dc9cc12f4cc55ed2b009ad
hktrollgames <hktrollgames@gmail.com>
2021-08-11 20:51:43 +0900

    웨이브 시스템 추가함

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public int spawnCount = 10;
-    public GameObject monster;
-    
-    void Start()
+    public int currentWaveIndex;
+
+    [System.Serializable]
+    public class WaveInfo
+    {
+        public int spawnCount = 10;
+        public GameObject monster;
+        public float time;
+    }
+    public List<WaveInfo> waves;
+
+    IEnumerator Start()
     {
         var spawnPoints = GetComponentsInChildren<SpawnPoint>(true);
-        for (int i = 0; i < spawnCount; i++)
+        foreach (var item in waves)
         {
-            int spawnIndex = Random.Range(0, spawnPoints.Length);
-            Vector3 spawnPoint = spawnPoints[spawnIndex].transform.position;
-            Instantiate(monster, spawnPoint, Quaternion.identity);
+            Debug.LogWarning($"{++currentWaveIndex} 시작됨");
+            int spawnCount = item.spawnCount;
+            for (int i = 0; i < spawnCount; i++)
+            {
+                int spawnIndex = Random.Range(0, spawnPoints.Length);
+                Vector3 spawnPoint = spawnPoints[spawnIndex].transform.position;
+                Instantiate(item.monster, spawnPoint, Quaternion.identity);
+            }
+
+            float nextWaveStartTime = Time.time + item.time;
+
+            while (Time.time < nextWaveStartTime)
+                yield return null;
         }
     }
 }
