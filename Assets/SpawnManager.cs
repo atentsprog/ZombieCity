909f11ffc5964ed9f616680e3ec515743643610f
hktrollgames <hktrollgames@gmail.com>
2021-08-11 21:45:51 +0900

    몬스터 리젠 할때 랜덤한 시간 간격을 두고 리젠 시킴

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -28,6 +28,7 @@ public class SpawnManager : SingletonMonoBehavior<SpawnManager>
     {
         nextWaveStartTime = 0;
     }
+    public float randomRegenDelayMax = 0.5f;
     IEnumerator Start()
     {
         var spawnPoints = GetComponentsInChildren<SpawnPoint>(true);
@@ -36,12 +37,15 @@ public class SpawnManager : SingletonMonoBehavior<SpawnManager>
             Debug.LogWarning($"{++currentWaveIndex} 시작됨");
             int spawnCount = item.spawnCount;
             for (int i = 0; i < spawnCount; i++)
-            {
+            { 
                 int spawnIndex = Random.Range(0, spawnPoints.Length);
                 Vector3 spawnPoint = spawnPoints[spawnIndex].transform.position;
-                var monster = item.monsters.OrderBy(x => Random.Range(0, x.ratio)).Last().monster;
-                //var monster = item.monsters.OrderByDescending(x => Random.Range(0, x.ratio)).First().monster;
+                var monster = item.monsters.OrderBy(x => Random.Range(0, x.ratio))
+                    .Last().monster;
+                //var monster = item.monsters.OrderByDescending(x => Random.Range(0, x.ratio))
+                //    .First().monster;
                 Instantiate(monster, spawnPoint, Quaternion.identity);
+                yield return new WaitForSeconds(Random.Range(0, randomRegenDelayMax));
             }
             nextWaveStartTime = Time.time + item.time;
 
