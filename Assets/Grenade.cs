2e019131a895dce1181a4db5a58d8c34c7689ea5
hktrollgames <hktrollgames@gmail.com>
2021-08-12 16:48:03 +0900

    수류탄 구현중

---
+++ Assets/Grenade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public ProjectileArc projectileArc;
+    public Transform firePoint;
+    public Cursor cursor;
+    public float speed = 20;
+    void Start()
+    {
+        projectileArc = GetComponent<ProjectileArc>();
+        firePoint = transform;
+        cursor = FindObjectOfType<Cursor>();
+    }
+
+    void Update()
+    {
+        SetTargetWithSpeed(cursor.transform.position, speed);
+    }
+
+    public float currentAngle;
+    public void SetTargetWithSpeed(Vector3 point, float speed)
+    {
+        Vector3 direction = point - firePoint.position;
+        float yOffset = direction.y;
+        direction = Math3d.ProjectVectorOnPlane(Vector3.up, direction);
+        float distance = direction.magnitude;
+
+        float angle0, angle1;
+        bool targetInRange = ProjectileMath.LaunchAngle(speed, distance, yOffset, Physics.gravity.magnitude, out angle0, out angle1);
+
+        if (targetInRange)
+            currentAngle =  angle0;
+
+        projectileArc.UpdateArc(speed, distance, Physics.gravity.magnitude, currentAngle, direction, targetInRange);
+    }
+}
